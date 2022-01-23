23bacdf05842792176d052ea3df95546bccc626d
Nikhil Manglore <nikhilmanglore9@gmail.com>
2022-01-23 01:46:15 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Nikhil Manglore on 1/23/22.
+//  Copyright © 2022 Nikhil Manglore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
